da03941a6217d96dae355469f871fc76601e994b
yo-nakano <ynakano7621@gmail.com>
2022-06-20 22:12:05 +0900

    テンプレート作成

--- Python/field_up/site_app/views.py
+++ Python/field_up/site_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'sample_app/index.html', {})
+    return render(request, 'site_app/index.html', {})
--- Python/venv/Scripts/activate.bat
+++ Python/venv/Scripts/activate.bat
@@ -8,7 +8,8 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Yo Nakano\Python\venv
+rem set VIRTUAL_ENV=C:\Users\Yo Nakano\Python\venv
+set VIRTUAL_ENV=C:\xampp\htdocs\Django\system\Python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
