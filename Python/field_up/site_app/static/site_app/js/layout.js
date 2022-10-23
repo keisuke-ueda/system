cc034ff5e52a3e04f979e7e7fa9838ed09823e86
keisuke-ueda <81659758+keisuke-ueda@users.noreply.github.com>
2022-10-23 18:53:49 +0900

    不要分削除

--- Python/field_up/site_app/static/site_app/js/layout.js
+++ Python/field_up/site_app/static/site_app/js/layout.js
@@ -80,20 +80,6 @@ $(function(){
 	});
 });
 
-// ローディングアニメーション
-$(function(){
-	var loader = $('.loader-wrap');
-
-	//ページの読み込みが完了したらアニメーションを非表示
-	$(window).on('load',function(){
-		loader.fadeOut();
-	});
-
-	//ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
-	setTimeout(function(){
-		loader.fadeOut();
-	},3000);
-});
 
 
 // デジタル文字
